11435aba0a7d314d57f6b9191739da47e8a8d8de
esp23 <01234567891989@mail.ru>
2014-01-23 17:41:28 +0400

    Добавление проекта

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style/style.css" />
+	</head>
+
+	<body>
+		<H3 align=center>Пример авторизации пользователя</H3>
+		<form action=page1.php class="login">
+			<p>Логин</p>
+			<input type=text/ name="login" required pattern="[0-9a-zA-z]{5,}"/><br>
+			<p>Пароль</p>
+			<input type=password required name="passwd"/>
+			<input type=submit id="submit" value="войти"/>
+			<a href="reg.php">Регистрация</a>
+		</form>
+	</body>
+</html>
+
+<?php
+#phpinfo();
+?>
